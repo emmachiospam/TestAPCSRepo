f0082fc7c90cf40bd4007c82a5102971e1a80beb
emmachio <emmany2409@gmail.com>
2020-10-21 01:04:25 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+
+
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      cw9.printLoop(Integer.parseInt(args[0]));
+    }
+    else {
+      cw9.printLoop(5);
+    }
+  }
+
+
+}
