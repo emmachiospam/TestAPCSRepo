9c8de932cb05464883331661e2aefb30c46e3b5b
emmachio <emmany2409@gmail.com>
2020-10-23 03:06:39 -0400

    Done

--- Demo.java
+++ Demo.java
@@ -2,12 +2,13 @@ public class Demo {
 
 
   public static void main(String[] args) {
-    if (args.length > 0) {
-      cw9.printLoop(Integer.parseInt(args[0]));
-    }
-    else {
-      cw9.printLoop(5);
-    }
+    // if (args.length > 0) {
+    //   cw9.printLoop(Integer.parseInt(args[0]));
+    // }
+    // else {
+    //   cw9.printLoop(5);
+    // }
+    System.out.println(arrayDeepToString(create2DArrayRadomized(6,10,3)).replaceAll("}, ","}\n"));
   }
 
   public static String arrToString(int[] arr){
@@ -50,4 +51,20 @@ public class Demo {
     }
     return array;
   }
+
+  public static int[][] create2DArrayRadomized(int rows, int cols, int maxValue) {
+    int[][] array = new int[rows][];
+    for(int k = 0; k < rows; k++) {
+      int cols2 = (int)Math.round(Math.random() * (cols + 1));
+      int[] arrCol = new int[cols2];
+      array[k] = arrCol;
+      for(int j = 0; j < cols2; j++) {
+        int x = (int)Math.round(Math.random() * (maxValue));
+        array[k][j] = x;
+      }
+    }
+    return array;
+  }
+
+
 }
